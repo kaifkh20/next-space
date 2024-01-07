1b7895c54910da84dcf49850efb7ae02538600bd
kaifkh20 <kaifkhan.biz@gmail.com>
2024-01-07 18:01:51 +0530

    NextSpace auth completion

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export const dynamic = 'force-static';
+
+export default function About(){
+
+    return (
+        <main>
+            <h1>About Us</h1>
+            <h4>We are a social media company</h4>
+        </main>
+    )
+}
