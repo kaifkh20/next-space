4edd3bd7bf3968e854a86aa19c25a64cec7cf6bb
kaifkh20 <kaifkhan.biz@gmail.com>
2024-01-15 11:48:53 +0530

    Following System

---
+++ app/users/[id]/loading.tsx
@@ -0,0 +1,7 @@
+export default function LoadingUser(){
+    return (
+        <div>
+            Loading User Data...
+        </div>
+    )
+}
---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,39 @@
+import FollowButton from '@/app/components/FollowButton/FollowButton';
+import { prisma } from '@/lib/prisma';
+import { Metadata } from 'next';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+  return { title: `User profile of ${user?.name}` };
+}
+
+export default async function UserProfile({ params }: Props) {
+  const user = await prisma.user.findUnique({ where: { id: params.id } });
+  const { name, bio, image, id,email } = user ?? {};
+
+//   throw new Error("This is example error")
+
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      <img
+        width={300}
+        src={image ?? '/mememan.webp'}
+        alt={`${name}'s profile`}
+      />
+
+      <h3>Bio</h3>
+      <p>{bio}</p>
+      <h3>Email</h3>
+      <p>{email}</p>
+      <FollowButton targetUserId={params.id}/>
+    </div>
+  );
+}
